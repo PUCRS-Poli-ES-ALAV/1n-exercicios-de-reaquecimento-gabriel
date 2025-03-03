bbf9cb0e77f5578df0b87976816c9b230cb07e29
Gabriel Stefanello <85457993+GabrielMattos7@users.noreply.github.com>
2025-03-03 10:57:05 -0300

    Update ex9.py

--- ex9.py
+++ ex9.py
@@ -1,2 +1,18 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-print(alphabet[:4])
+alphabet = "abcdefghijklmnopqrstuvwxyz".upper()
+# print(alphabet[:4])
+
+def permut(list, prefix=""):
+    if not list:
+        print(prefix)
+    else:
+        for i in range(len(list)):
+            actual_prefix = prefix + list[i]
+            restante = list[:i] + list[i+1:]
+            permut(restante, actual_prefix)
+
+def func(n):
+    leters = list(alphabet[:n])
+    permut(leters)
+
+
+func(3)
